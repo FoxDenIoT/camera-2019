16dda7ce8a0abf941fe99e0ce237a27c6aaa03b5
tejaswigowda <tejaswi@foxyninjastudios.com>
2019-06-16 11:14:02 -0700

    ac

---
+++ docs/day1/helloWorld/helloWorld.ino
@@ -0,0 +1,35 @@
+#include "SSD1306.h"
+#include "OLEDDisplayUi.h"
+#define SSD1306_ADDRESS 0x3c
+
+#define I2C_SDA 21
+#define I2C_SCL 22
+
+SSD1306 oled(SSD1306_ADDRESS, I2C_SDA, I2C_SCL);
+OLEDDisplayUi ui(&oled);
+
+
+
+//FrameCallback frames[] = {drawFrame1, drawFrame2};
+#define FRAMES_SIZE (sizeof(frames) / sizeof(frames[0]))
+
+
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+    Serial.println();
+
+
+    oled.init();
+    oled.setFont(ArialMT_Plain_16);
+    oled.setTextAlignment(TEXT_ALIGN_CENTER);
+    delay(50);
+    oled.drawString(oled.getWidth() / 2, oled.getHeight() / 2, "HELLO WORLD");
+    oled.display();
+}
+
+void loop(){
+
+}
