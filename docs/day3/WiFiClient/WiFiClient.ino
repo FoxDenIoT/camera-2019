520ea8ed74db18a3ad1eb90577cc9b0c9387bf99
tejaswigowda <tejaswi@foxyninjastudios.com>
2019-06-19 11:14:48 -0700

    ac

--- docs/day3/WiFiClient/WiFiClient.ino
+++ docs/day3/WiFiClient/WiFiClient.ino
@@ -1,12 +1,17 @@
 /*
- *  This sketch sends a message to a TCP server
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
  *
  */
 
 #include <WiFi.h>
-#include <WiFiMulti.h>
 
-WiFiMulti WiFiMulti;
+const char* ssid     = "Tejaswi3";
+const char* password = "12345678";
+
+const char* host = "google.com";
 
 void setup()
 {
@@ -14,67 +19,68 @@ void setup()
     delay(10);
 
     // We start by connecting to a WiFi network
-    WiFiMulti.addAP("Tejaswi3", "12345678");
 
     Serial.println();
     Serial.println();
-    Serial.print("Wait for WiFi... ");
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
 
-    while(WiFiMulti.run() != WL_CONNECTED) {
-        Serial.print(".");
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
         delay(500);
+        Serial.print(".");
     }
 
     Serial.println("");
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
-
-    delay(500);
 }
 
+int value = 0;
 
 void loop()
 {
-    const uint16_t port = 80;
-    const char * host = "google.com"; // ip or dns
-
-
+    delay(5000);
+    ++value;
 
     Serial.print("connecting to ");
     Serial.println(host);
 
     // Use WiFiClient class to create TCP connections
     WiFiClient client;
-
-    if (!client.connect(host, port)) {
+    const int httpPort = 80;
+    if (!client.connect(host, httpPort)) {
         Serial.println("connection failed");
-        Serial.println("wait 5 sec...");
-        delay(5000);
         return;
     }
 
+    // We now create a URI for the request
+    String url = "/"
+
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
+
     // This will send the request to the server
-    client.print("Send this data to server");
-
-    while (client.connected()) {
-      String line = client.readStringUntil('\n');
-      if (line == "\r") {
-        Serial.println("headers received");
-        break;
-      }
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
     }
-    // if there are incoming bytes available
-    // from the server, read them and print them:
-    while (client.available()) {
-      char c = client.read();
-      Serial.write(c);
-    }
-    
 
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+    }
+
+    Serial.println();
     Serial.println("closing connection");
-    client.stop();
-
-    Serial.println("wait 5 sec...");
-    delay(5000);
 }
